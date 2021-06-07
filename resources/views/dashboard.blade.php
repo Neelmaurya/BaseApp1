c9bb93f01f2f8702c671bd504415a5c1769d6767
Neelmaurya <81714035+iqburn@users.noreply.github.com>
2021-06-07 23:50:26 +0530

    ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
@@ -13,7 +13,7 @@
         <form action="{{ route('file.upload.post')}}" method="POST" enctype="multipart/form-data">
             @csrf
                 <table><tr>
-                    <td><input type="file" name="file" class="form-control"></td>
+                    <td><input type="file" name="file" class="form-control px-5 py-2 font-bold rounded"></td>
                     <td><button type="submit" class="px-5 py-2 bg-green-400 border-blue-500 border text-black font-bold rounded transition duration-300 hover:bg-green-600 hover:text-white">Upload</button></td>
                     </tr>
                 </table>
@@ -34,10 +34,10 @@
         </tr>
         <tr class="border-2 border-black">
             @if($name = Session::get('file'))
-             <td  style="width: 45%;text-align: left;">{{$name}}</td>
+             <td class="font-semibold pt-4 text-lg"  style="width: 45%;text-align: left;">{{$name}}</td>
             @endif
             @if($sizes = Session::get('size'))
-             <td style="width: 20%;text-align: right;">{{$sizes}}byte</td>
+             <td class="font-semibold pt-4 text-lg" style="width: 20%;text-align: right;">{{$sizes}}byte</td>
             @endif
              <td style="width: 30%;">
                
